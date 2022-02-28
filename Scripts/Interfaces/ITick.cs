c0798f29b405483e03453c0d5e84e3f1a9e6dde3
Allen Oliver <100629538+Gnosis-Dev@users.noreply.github.com>
2022-02-28 16:49:48 -0500

    Adding Tick interface

---
+++ Scripts/Interfaces/ITick.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface ITick
+{
+    void Tick();
+
+    void FixedTick();
+
+    void LateTick();
+}
